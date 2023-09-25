12abe9cab7a3c27ddb2cd4e1db43ad4bf903eafb
Ayush Jain <ayushjain2205@gmail.com>
2023-09-25 18:40:37 +0530

    add navbar scaffold

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -2,28 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 p-0 m-0">
+    <nav className="flex justify-between items-center h-[56px]">
       <div className="flex-1">
-        <span>TRON.ai</span>
+        <span className="text-[32px] font-bold">TRON.ai</span>
       </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
+      <div className="flex flex-row gap-[24px]">
+        <button>Connect wallet</button>
+        <span>avatar</span>
       </div>
-    </div>
+    </nav>
   );
 };
 
