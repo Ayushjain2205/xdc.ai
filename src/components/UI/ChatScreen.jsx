30b63427832d94f16a25e31c77c266c603be414e
Ayush Jain <ayushjain2205@gmail.com>
2023-09-25 20:04:06 +0530

    Fix input box coming up bug

--- src/components/UI/ChatScreen.jsx
+++ src/components/UI/ChatScreen.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ChatScreen = () => {
   return (
     <div className="flex flex-col h-full px-[16px]">
-      <div className=" overflow-y-scroll"></div>
+      <div className="flex-1 overflow-y-scroll"></div>
       <div className="flex-none">
         <input className="border w-full mt-2" type="text" />
       </div>
