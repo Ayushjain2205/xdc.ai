1e88d2eb7bee9a45196748dd3bf9d4edb97f945e
Ayush Jain <ayushjain2205@gmail.com>
2023-09-25 18:25:34 +0530

    Init daisy

---
+++ src/components/UI/Layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return <div className="p-[32px]">{children}</div>;
+};
+
+export default Layout;
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Layout from "../components/UI/Layout";
+import Navbar from "../components/UI/Navbar";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Navbar />
+    </Layout>
+  );
+}
