3aee7d021e91ed0d1c8abc8d850b99402e327ac8
Ayush Jain <ayushjain2205@gmail.com>
2023-09-25 23:20:52 +0530

    Style prompt box

--- src/components/UI/ChatScreen.jsx
+++ src/components/UI/ChatScreen.jsx
@@ -3,9 +3,20 @@ import React from "react";
 const ChatScreen = () => {
   return (
     <div className="flex flex-col h-full px-[16px]">
-      <div className="flex-1 overflow-y-scroll"></div>
+      <div className="flex-1 overflow-y-scroll">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nam.
+        Dolorem sapiente deserunt obcaecati quidem, autem dicta deleniti id quae
+        assumenda fuga odit commodi consequuntur. Unde tempore officia id quasi.
+      </div>
       <div className="flex-none">
-        <input className="border w-full mt-2" type="text" />
+        <div className="flex flex-row items-center pr-[12px] rounded-[2px] border-[3px] border-[#E7E9EB] bg-transparent w-full h-[64px]">
+          <input
+            className="bg-transparent w-full p-[18px] focus:outline-none"
+            type="text"
+            placeholder="Send a prompt"
+          />
+          <img src="/icons/send.svg" className="h-[36px] w-[36px]" alt="" />
+        </div>
       </div>
     </div>
   );
