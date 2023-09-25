82b70282dc7733504dd958a9f3986a677a32f942
Ayush Jain <ayushjain2205@gmail.com>
2023-09-26 02:45:24 +0530

    Change dropdown icon also on theme change

--- src/components/UI/SideBar.jsx
+++ src/components/UI/SideBar.jsx
@@ -15,10 +15,11 @@ const SideBar = () => {
                 height="24"
                 viewBox="0 0 24 24"
                 fill="none"
+                className="text-primary"
               >
                 <path
                   d="M6 9L12 15L18 9"
-                  stroke="#D34D3E"
+                  stroke="currentColor"
                   stroke-width="2"
                   stroke-linecap="round"
                   stroke-linejoin="round"
