cbb32aa5e96fd0a9745fef71ddcc6fd05e1247f0
Ayush Jain <ayushjain2205@gmail.com>
2023-09-26 16:55:58 +0530

    Add related prompts and resources to AI message

--- src/components/UI/ChatScreen.jsx
+++ src/components/UI/ChatScreen.jsx
@@ -11,9 +11,36 @@ const UserMessage = ({ message }) => {
 
 const AIMessage = ({ message }) => {
   return (
-    <div className="flex flex-row items-start gap-[16px] pb-[24px] mb-[24px] border-b border-[#E7E9EB]">
-      <img src="/images/ai-message.svg" alt="AI" />
-      <p className="text-[16px]">{message}</p>
+    <div className=" pb-[24px] mb-[24px] border-b border-[#E7E9EB]">
+      <div className="flex flex-row items-start gap-[16px]">
+        <img src="/images/ai-message.svg" alt="AI" />
+        <p className="text-[16px]">{message}</p>
+      </div>
+      <div className="flex flex-col gap-[8px] mt-[30px]">
+        <p className="text-[16px] opacity-40">Resources:</p>
+        <div className="flex flex-row gap-[10px]">
+          <div className="flex flex-row items-center h-[32px] gap-[7px] px-[18px] rounded-[10px] border border-[#7A7A7A] text-[#7A7A7A]">
+            name of resource <img src="/icons/link.svg" alt="" />
+          </div>
+          <div className="flex flex-row items-center h-[32px] gap-[7px] px-[18px] rounded-[10px] border border-[#7A7A7A] text-[#7A7A7A]">
+            name of resource <img src="/icons/link.svg" alt="" />
+          </div>
+          <div className="flex flex-row items-center h-[32px] gap-[7px] px-[18px] rounded-[10px] border border-[#7A7A7A] text-[#7A7A7A]">
+            name of resource <img src="/icons/link.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-[8px] mt-[16px]">
+        <p className="text-[16px] opacity-40">Related prompts:</p>
+        <div className="flex flex-row gap-[10px]">
+          <div className="flex flex-row items-center h-[24px] gap-[7px] px-[8px] rounded-[4px] bg-white text-[#7A7A7A] text-[14px]">
+            How do I deploy a NFT contract?
+          </div>
+          <div className="flex flex-row items-center h-[24px] gap-[7px] px-[8px] rounded-[4px] bg-white text-[#7A7A7A] text-[14px]">
+            How do I debug a contract?
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
