c28dbcd8bdac3ee755834c5d9153c0e9433c1273
Ayush Jain <ayushjain2205@gmail.com>
2023-09-29 17:37:14 +0530

    Add popover for disconnect button

--- src/components/Functional/ConnectWallet.jsx
+++ src/components/Functional/ConnectWallet.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TronWeb from "tronweb";
+import * as Popover from "@radix-ui/react-popover";
 
 function ConnectWallet() {
   const [tronWeb, setTronWeb] = useState(null);
@@ -25,18 +26,28 @@ function ConnectWallet() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       {tronWeb ? (
-        <div className="flex items-center space-x-4">
-          <div className="flex items-center justify-center rounded-full bg-primary h-12 w-12">
-            <img src="/icons/dude.svg" alt="Avatar" />
-          </div>
-          <span className="text-gray-700">{tronWeb.defaultAddress.base58}</span>
-          <button
-            className="btn btn-primary rounded-none bg-primary h-[48px] w-[192px] font-bold text-white"
-            onClick={handleDisconnect}
-          >
-            Disconnect Wallet
-          </button>
-        </div>
+        <Popover.Root>
+          <Popover.Trigger asChild>
+            <div className="flex items-center space-x-4">
+              <div className="flex items-center justify-center rounded-full bg-primary h-12 w-12">
+                <img src="/icons/dude.svg" alt="Avatar" />
+              </div>
+              <span className="text-gray-700">
+                {tronWeb.defaultAddress.base58}
+              </span>
+            </div>
+          </Popover.Trigger>
+          <Popover.Portal>
+            <Popover.Content>
+              <button
+                className="btn btn-primary rounded-none bg-primary h-[48px] w-[192px] font-bold text-white"
+                onClick={handleDisconnect}
+              >
+                Disconnect Wallet
+              </button>
+            </Popover.Content>
+          </Popover.Portal>
+        </Popover.Root>
       ) : (
         <div className="flex flex-col items-center space-y-4">
           <button
