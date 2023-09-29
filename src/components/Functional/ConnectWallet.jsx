bd6024ed9ac00b4d629909efe16be628a2ad6123
Ayush Jain <ayushjain2205@gmail.com>
2023-09-29 16:13:46 +0530

    Init tron web and connext wallet logic

---
+++ src/components/Functional/ConnectWallet.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import TronWeb from "tronweb";
+
+function ConnectWallet() {
+  const [tronWeb, setTronWeb] = useState(null);
+
+  useEffect(() => {
+    if (window.tronWeb && window.tronWeb.ready) {
+      setTronWeb(window.tronWeb);
+    } else {
+      window.addEventListener("load", () => {
+        if (window.tronWeb && window.tronWeb.ready) {
+          setTronWeb(window.tronWeb);
+        } else {
+          console.warn("TronWeb is not installed. Install TronLink.");
+        }
+      });
+    }
+  }, []);
+
+  const handleDisconnect = () => {
+    setTronWeb(null);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      {tronWeb ? (
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center justify-center rounded-full bg-primary h-12 w-12">
+            <img src="/icons/dude.svg" alt="Avatar" />
+          </div>
+          <span className="text-gray-700">{tronWeb.defaultAddress.base58}</span>
+          <button
+            className="btn btn-primary rounded-none bg-primary h-[48px] w-[192px] font-bold text-white"
+            onClick={handleDisconnect}
+          >
+            Disconnect Wallet
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center space-y-4">
+          <button
+            className="btn btn-primary rounded-none bg-primary h-[48px] w-[192px] font-bold text-white"
+            onClick={() => {
+              if (window.tronWeb && window.tronWeb.ready) {
+                setTronWeb(window.tronWeb);
+              } else {
+                console.warn("TronWeb is not installed. Install TronLink.");
+              }
+            }}
+          >
+            Connect Wallet
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ConnectWallet;
