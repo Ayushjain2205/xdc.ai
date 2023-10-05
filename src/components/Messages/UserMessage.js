336f6a729f21ea4a824d6f8c84b8c646acbe5808
Ayush Jain <ayushjain2205@gmail.com>
2023-10-05 12:33:28 +0530

    Modify user message style

--- src/components/Messages/UserMessage.js
+++ src/components/Messages/UserMessage.js
@@ -2,7 +2,7 @@ const UserMessage = ({ message }) => {
   return (
     <div className="flex flex-row items-start gap-[16px] pb-[16px] mb-[16px]">
       <img src="/images/user-message.svg" className="h-[36px]" alt="User" />
-      <p className="text-[20px] font-bold">{message}</p>
+      <p className="text-[16px] font-bold">{message}</p>
     </div>
   );
 };
